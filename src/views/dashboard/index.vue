1bdbd544008b2e7d95bcde890dde81bf1cfd26ce
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-27 19:09:35 +0800

    [*]header

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,8 +3,8 @@
     <!-- <div class="dashboard-text">name:{{ name }}</div> -->
     <!-- <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div> -->
     <div id="container" style="position:relative;background-color:rgb(0,188,150);">
-      <div style="margin-top:20px;text-aligh:center;font-size: 42px;">即时而安全的代币间兑换</div>
-      <div style="text-aligh:center;font-size: 36px;">无需订单，无需充值，无需包装</div>
+      <div style="padding:60px 0 20px;text-align:center;font-size: 42px;color:#fff;">即时而安全的代币间兑换</div>
+      <div style="padding-bottom:40px;text-align:center;font-size: 36px;color:#fff;">无需订单，无需充值，无需包装</div>
       <el-button id="button" v-if="!isMove" v-on:click="onAcceptButtonTap">接受条款和协议</el-button>
     </div>
     <div id="center-boxes" :style="{transform}" >
@@ -24,7 +24,7 @@ export default {
   data(){
     return{
       isMove:false,
-      transform: 'translateY(60%)'
+      transform: 'translateY(40%)'
     }
   },
   computed: {
@@ -56,19 +56,18 @@ export default {
 #container{
   background-repeat: no-repeat;
   background-size:100%;
-  height:400px; 
-  display:flex; 
-  justify-content:center; 
-  align-items:center;
+  height:540px; 
 }
 
 #button{
+  display: block;
     width: 180px;
     height: 58px;
     background: #fff;
     color: #00c69a;
     font-weight: 600;
     font-size: 18px;
+    margin: 0 auto;
     border-radius: 40px;
 }
 
@@ -82,7 +81,7 @@ export default {
   position:absolute; 
   flex-wrap: wrap;
   justify-content: center;
-  top:20%;
+  top:30%;
   transition:transform 2s;
   left:0;
 }
@@ -94,6 +93,6 @@ export default {
   margin:20px;
   opacity: 0.5;
   border-radius: 20px;
-  box-shadow:0px 1px 4px 1px rgb(28,28,28);
+  box-shadow:0px 1px 4px 1px rgba(28,28,28,0.2);
 }
 </style>
