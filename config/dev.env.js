18551ed2b61b4b78f873f0b7f72350c9cf434897
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-27 13:08:57 +0800

    [*]

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080/user/admin"',
+  BASE_API: '"https://easy-mock.com/mock/5bd3e368694d1a079e769911"',
 })
